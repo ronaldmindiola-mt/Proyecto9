a7e38f822f75f6f49f4c87344a074262df477394
Ronald Mindiola <ronaldmindiola@live.com>
2022-10-17 14:51:08 -0500

    run 3

--- src/main/java/com/usa/mintic/reto/entities/Bike.java
+++ src/main/java/com/usa/mintic/reto/entities/Bike.java
@@ -30,67 +30,51 @@ public class Bike implements Serializable {
     @JsonIgnoreProperties({"bike", "messages"})
     private List<Reservation> reservations;
 
-
     public Integer getId() {
         return id;
     }
-
     public void setId(Integer id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getBrand() {
         return brand;
     }
-
     public void setBrand(String brand) {
         this.brand = brand;
     }
-
     public Integer getYear() {
         return year;
     }
-
     public void setYear(Integer year) {
         this.year = year;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
-
     public Category getCategory() {
         return category;
     }
-
     public void setCategory(Category category) {
         this.category = category;
     }
-
     public List<Message> getMessages() {
         return messages;
     }
-
     public void setMessages(List<Message> messages) {
         this.messages = messages;
     }
-
     public List<Reservation> getReservations() {
         return reservations;
     }
-
     public void setReservations(List<Reservation> reservations) {
         this.reservations = reservations;
     }
